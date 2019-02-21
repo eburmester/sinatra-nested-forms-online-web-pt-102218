1518c0154b75f7244980d5288cec46ff765d93e5
Elyse Burmester <burmesterelyse@gmail.com>
2019-02-21 19:33:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,8 +12,15 @@ module FormsLab
     end 
     
     post '/pirates' do
-      
-      erb :show 
+      params["pirate"]["ships"].each do |ship_data|
+        Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
+      end
+
+      @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
+
+      @ships = Ship.all
+
+      erb :'pirates/show'
     end 
   end
 end
