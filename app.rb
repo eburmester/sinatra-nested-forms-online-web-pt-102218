2c8ee17e08f4ef9e5c4b316043b708743323a94e
Elyse Burmester <burmesterelyse@gmail.com>
2019-02-21 19:31:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,18 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
-    # code other routes/actions here
-
+    
+    get '/' do 
+      erb :index 
+    end
+    
+    get '/new' do 
+      erb :new 
+    end 
+    
+    post '/pirates' do
+      
+      erb :show 
+    end 
   end
 end
